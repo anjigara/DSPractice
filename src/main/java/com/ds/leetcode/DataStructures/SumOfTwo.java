8b0a278b0dfe99f15af9359b60ffccc28bbd3be3
anjigara <anjigara@gmail.com>
2024-01-13 12:55:01 +0530

    Added Problem-1:SumOfTwo

--- src/main/java/com/ds/leetcode/DataStructures/SumOfTwo.java
+++ src/main/java/com/ds/leetcode/DataStructures/SumOfTwo.java
@@ -1,6 +1,8 @@
 package com.ds.leetcode.DataStructures;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -36,10 +38,40 @@ import java.util.Arrays;
 
 public class SumOfTwo {
   public static void main(String[] args) {
+    //Option-1
     int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
     System.out.println("Result:: " + Arrays.toString(result));
+    //Option-2
+    int[] result1 = twoSumWithMap(new int[]{3, 2, 4}, 7);
+    System.out.println("Map Result:: " + Arrays.toString(result1));
   }
 
+  /**
+   * This method complexity is O(n)
+   * @param ints
+   * @param target
+   * @return
+   */
+  private static int[] twoSumWithMap(int[] ints, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int[] result = new int[2];
+    for (int i = 0; i < ints.length; i++) {
+      if (map.containsKey(target - ints[i])) {
+        result[0] = map.get(target - ints[i]);
+        result[1] = i;
+      }
+      map.put(ints[i], i);
+    }
+    return result;
+  }
+
+
+  /**
+   * This method complexity is O(n^2)
+   * @param nums
+   * @param target
+   * @return
+   */
   public static int[] twoSum(int[] nums, int target) {
     int[] result = new int[2];
     for (int i = 0; i < nums.length; i++) {
@@ -48,7 +80,6 @@ public class SumOfTwo {
           result[0] = i;
           result[1] = j;
           return result;
-
         }
       }
     }
